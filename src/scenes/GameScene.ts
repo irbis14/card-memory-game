ef4f23a3ee2a7a37f680e912e731d33edd54cb00
irbis14 <nina.motorna@gmail.com>
2023-12-28 16:41:45 +0100

    adds timer

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -2,6 +2,7 @@ import { Scene } from 'phaser';
 import { Card } from '../Card';
 import { CardDealer } from '../CardDealer';
 import { MemoDOM } from '../MemoDOM';
+import { Timer } from '../Timer';
 
 type SceneCreateProps = {
   isRestart?: boolean;
@@ -12,9 +13,14 @@ export class GameScene extends Scene {
 
   private _menuDOM: MemoDOM;
 
+  private _timer: Timer;
+
+  private _isGameOver: boolean;
+
   onStartGame = async () => {
     await this._cardDealer.createCards();
     this.input.on('gameobjectdown', this.onCardClick);
+    this._timer.start();
   };
 
   onRestartGame = () => {
@@ -22,11 +28,19 @@ export class GameScene extends Scene {
   };
 
   onCardClick = (_: unknown, card: Card) => {
+    if (this._isGameOver) return;
     this._cardDealer.openCard(card);
   };
 
   onAllCardsOpen = () => {
     this._menuDOM.render({ type: 'end', isWin: true });
+    this._timer.stop();
+    this._isGameOver = true;
+  };
+
+  onTimerIsOver = () => {
+    this._menuDOM.render({ type: 'end', isWin: false });
+    this._isGameOver = true;
   };
 
   constructor() {
@@ -40,7 +54,11 @@ export class GameScene extends Scene {
     //    card.scale = 0.6;
     // 2) const card = new Card(this, { x: 250, y: 300, id: '2' });
 
+    this._isGameOver = false;
+
     this._cardDealer = new CardDealer(this);
+
+    this._timer = new Timer(this, { x: 600, y: 10, maxTime: 30 });
 
     this._menuDOM = new MemoDOM();
 
@@ -56,5 +74,6 @@ export class GameScene extends Scene {
     this._menuDOM.onRestartGame = this.onRestartGame;
     this._cardDealer.onAllCardsOpen = this.onAllCardsOpen;
     // this.input.on('gameobjectdown', this.onCardClick);
+    this._timer.onTimeIsOver = this.onTimerIsOver;
   }
 }
